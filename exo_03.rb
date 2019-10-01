bf09b90a5060f51aa74c1a8b3b461166181de2da
guillaume <gueyome@hotmail.fr>
2019-10-01 17:13:17 +0200

    last_commit

--- exo_03.rb
+++ exo_03.rb
@@ -1,5 +1,5 @@
 declaration = "Bonjour, monde !"
-declaration_2 = "Et avec une voix sexy, ça donne : "
+#declaration_2 = "Et avec une voix sexy, ça donne : "
     puts declaration 
     print declaration_2 
     puts declaration
