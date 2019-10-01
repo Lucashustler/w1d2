8fb780f658138b14154cd2a44f3a444046fcabb9
Lucas Grandviergne <lucasgdve@gmail.com>
2019-10-01 17:09:21 +0200

    Please enter the commit message for your changes. Lines starting  exo 21

--- exo_21.rb
+++ exo_21.rb
@@ -4,10 +4,15 @@ nombre = gets.chomp.to_i
 i = 1
 
 while i <= nombre
+    (nombre-i).times do
+        print " "
+    end
     i.times do
         print "#"
-    end
+    end 
     i = i + 1
     puts ""
+    
+
 end
 
