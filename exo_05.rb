a0dd7ce1c6d49fdd84d0685bf6c40b9e865dd558
Lucas Grandviergne <lucasgdve@gmail.com>
2019-10-01 11:41:26 +0200

    lucas commit

---
+++ exo_05.rb
@@ -0,0 +1,24 @@
+puts "On va compter le nombre d'heures de travail à THP"
+#calcul chaine de caractère / caclcul charge de travabil en heures
+puts "Travail : #{10 * 5 * 11}" 
+#calcul chaine de caractère / caclcul charge de travabil en minutes
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" 
+#afficher une chaine de caractère / charge de travail en secondes
+puts "Et en secondes ?" 
+
+puts 10 * 5 * 11 * 60 * 60
+# boolean / affiche résultat d'un test logique
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+#affiche résulat opération
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#affiche phrase simple
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+# #affiche résulat test logique incluant opération
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
